85b2453e3979dff270c1ab614505a23e70a1cc78
Benjamin <ayzenbergb@gmail.com>
2017-06-19 13:56:34 -0700

    basic server

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 'use strict';
+
+require('dotenv').load();
+
+const express = require('express');
+const cors = require('cors');
+const Promise = require('bluebird');
+const errorHandler = require('./lib/error-middleware.js');
+const bodyParser = require('body-parser').json();
+const mongoose = require('mongoose');
+
+const app = module.exports = express();
+const router = express.Router();
+const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/lfg-dev';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.use(errorHandler);
+app.use(cors());
+app.use(bodyParser);
+app.use('/api', require('./router/auth-routes.js')(router));
+
+app.listen(PORT, () => console.log(`Hosted on localhost:${PORT}`));
