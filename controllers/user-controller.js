b2dfd092517b8fbd02600ab30022b98d634128bd
Michael Padget <mpadget@comcast.net>
2017-06-21 10:31:47 -0700

    Working user object with bearer auth

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,40 +1,27 @@
 'use strict';
 
 const createError = require('http-errors');
-// const Promise = require('bluebird');
-const User = require('../model/user.js');
+// const User = require('../model/user.js');
 
 module.exports = exports = {};
 
 exports.getUser = function(req) {
-  // Working basic auth.
-  // if(!req.auth.username) return Promise.reject(createError(400, 'Invalid username'));
-  //
-  // if(!req.auth.password) return Promise.reject(createError(400, 'Invalid password'));
-  //
-  // return User.findOne({username: req.auth.username})
-  // .then(user => user.comparePasswordHash(req.auth.password))
-  // .then(user => user.generateToken())
-  // .then(token => token)
-  // .catch(err => createError(404, err.message));
+  if(!req.user) return Promise.reject(createError(400, 'Invalid username'));
+  // Returns all object properties and sets some to null.
+  req.user.password = null;
+  req.user.findhash = null;
+  req.user.__v = null;
+  return Promise.resolve(req.user);
 
-  // console.log('Log request: ', req.body);
-  // return User.findOne({username: req.body})
-  // User.findOne({username: req.body})
-  // User.findById(req.params.id);
-  console.log('Log req.params.id', req.params.id);
-  console.log('Log User', User.findOne({username: req.body}));
-  // .then(user => res.json(user));
-  // .catch(err => createError(404, err.message));
+  // Returns only username and password.
+  // let userObject = {
+  //   username: req.user.username,
+  //   email: req.user.email,
+  // };
+  // return Promise.resolve(userObject);
 };
 
 // Check that user is logged in, if not route to signin.
 // GET request for that users information req.body
 // PUT request to update any updates from user on submit.
 // DELETE user will not currently be a supported feature.
-
-// User model.
-// const userSchema = Schema({
-//   username: {type: String, required: true, unique: true},
-//   email: {type: String, required: true, unique: true},
-//   password: {type:String, required:true},
--- router/user-routes.js
+++ router/user-routes.js
@@ -1,73 +1,16 @@
 'use strict';
 
-// const Router = require('express').Router;
-// const createError = require('http-errors');
-// const User = require('../models/user');
 const userCtrl = require('../controllers/user-controller');
-// const debug = require('debug')('ltg:user-routes');
+const debug = require('debug')('ltg:user-routes');
 const bearerAuth = require('../lib/bearer-auth-middleware');
-// const basicAuth = require('../lib/basic-auth-middleware');
 
-// 1 server > 2 user-routes > 3 user-controller
 module.exports = function(router) {
-  // Working basic auth.
-  // router.get('/user', basicAuth, (req, res) => {
-    // userCtrl.getUser(req)
-    // .then(token => res.json(token))
-    // .catch(err => res.status(err.status).send(err.message));
-
-    // console.log('Log request: ', req);
-    // req object IncomingMessage is very long.
-    // req.body is an empty object.
-
-    // console.log('Log response: ', res);
-    // res object ServerResponse is also very long.
-    // params: {}, query: {}, body: {} are empty.
-    // auth: { username: '', password: '' } }
-  // });
-
   router.get('/user', bearerAuth, (req, res) => {
+    debug('ltg:user-routes');
     return userCtrl.getUser(req)
-    .then(token => res.json(token))
+    .then(userObject => res.json(userObject))
     .catch(err => res.status(err.status).send(err.message));
   });
 
   return router;
 };
-
-/* // 17 gallery-routes
-  router.get('/gallery/:id', bearerAuth, (req, res) => {
-    debug('#GET /api/gallery/:id');
-
-    return Gallery.findById(req.params.id)
-    .then(gallery => {
-      if(gallery.userId.toString() !== req.user._id.toString()) {
-        return createError(401, 'Invalid user');
-      }
-      res.json(gallery);
-    })
-    .catch(err => res.status(err.status).send(err.message));
-  });
-*/
-
-/* // 27 gallery-router
-galleryRouter.get('/api/gallery/:id', bearerAuth, itemQueries,  function(req, res, next){
-  debug('GET /api/gallery/:id')
-  Gallery.findById(req.params.id)
-  .populate({
-    path: 'pics',
-    options: {
-      sort: {_id: req.query.itemsort},
-      limit: req.query.itemcount,
-      skip: req.query.itemoffset,
-    },
-  })
-  .catch(err => Promise.reject(createError(400, err.message)))
-  .then(gallery => {
-    if (gallery.userID.toString() !== req.user._id.toString())
-      return Promise.reject(createError(401, 'invalid userid'))
-    res.json(gallery)
-  })
-  .catch(next)
-})
-*/
